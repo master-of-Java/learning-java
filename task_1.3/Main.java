597141e7971d4a707c2f3b26aff1b66c6cc3b6e1
master-of-Java <34694618+master-of-Java@users.noreply.github.com>
2020-11-07 00:52:26 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+                int a = 8;
+                int b = 2;
+                String s = "+";
+                String j = "=";
+
+                System.out.print(a);
+                System.out.print(s);
+                System.out.print(b);
+                System.out.print(j);
+                System.out.println(a+b);
+    }
+}
