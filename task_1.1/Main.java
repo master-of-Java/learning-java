fa5a049d9e460bccd361b2048679bc3128d90a86
master-of-Java <34694618+master-of-Java@users.noreply.github.com>
2020-11-07 00:33:39 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Александр";
+        int age = 22;
+        String city = "Москва";
+        city = "Москвы";
+        System.out.println("Привет, " +"меня зовут " +name + ", " +"мне " +age +" года, " +"я из " +city);
+
+    }
+}
