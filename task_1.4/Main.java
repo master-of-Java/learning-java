101fa6260bf0773f9ad87646ff043e813149c33c
master-of-Java <34694618+master-of-Java@users.noreply.github.com>
2020-11-07 22:38:50 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 25;
+        String a = "квадрат";
+        String b = "числа";
+        String c = "равен";
+
+
+        System.out.println(a+" "+b+ " "+number+" "+c+" "+number*number);
+
+    }
+}
